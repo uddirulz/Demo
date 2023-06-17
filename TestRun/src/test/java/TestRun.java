121cb3d6a5a6428dedf418ef1274746ea9428133
Uddipan Halder <uddirulz@gmail.com>
2023-06-17 13:21:05 +0100

    First Commit
    Signed-off-by: Uddipan Halder<uddirulz@gmail.com>

---
+++ TestRun/src/test/java/TestRun.java
@@ -0,0 +1,20 @@
+import static us.abstracta.jmeter.javadsl.JmeterDsl.*;
+
+import org.junit.jupiter.api.Test;
+
+public class TestRun {
+	
+	@Test
+	public void testRun() throws Exception{
+		
+		testPlan(threadGroup(1, 10)
+				.children(transaction("Launch")
+						.children(httpSampler("https://blazedemo.com/index.php")
+									.children(responseAssertion("Welcome")))
+						.generateParentSample(true)
+						),
+				jtlWriter("target/jtl")
+				).run();
+	}
+
+}
