58064cf1b0fe0db5eac2ad8ce691eede7e1f7287
uddirulz <95777688+uddirulz@users.noreply.github.com>
2023-06-26 22:21:19 +0530

    Create entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+echo "Execution is being started"
+echo "**************************"
+jmeter $@
+echo "**************************"
+echo "Execution has been completed, please check the artifacts to download the results."
+
